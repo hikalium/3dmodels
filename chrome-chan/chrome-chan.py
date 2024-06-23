3bad444095ac3222824602e105f31a544afd0f91
hikalium <hikalium@hikalium.com>
2024-06-23 18:48:45 +0900

    update

--- chrome-chan/chrome-chan.py
+++ chrome-chan/chrome-chan.py
@@ -15,19 +15,61 @@ while(len(bpy.data.meshes)):
 while(len(bpy.data.objects)):
     bpy.data.objects.remove(bpy.data.objects[0])
 
-print("hello");
+bpy.ops.mesh.primitive_uv_sphere_add(segments=60,ring_count=30, radius=2.0)
+obj1 = bpy.context.active_object
+obj1.name = "sphere"
 
-verts = []
-verts.append([1, 1, 0])
-verts.append([1, -1, 0])
-verts.append([-1, 1, 0])
-verts.append([0, 0, 1])
-edges = []
-faces = []
-faces.append([1, 2, 3])
-faces.append([1, 2, 0])
 
-mesh = bpy.data.meshes.new(name = "mesh0")
-mesh.from_pydata(verts, edges, faces)
-obj = bpy.data.objects.new("object0", mesh)
-bpy.context.scene.collection.objects.link(obj)
+bpy.ops.mesh.primitive_cylinder_add(vertices=60, radius=1.0, depth=4.0)
+obj0 = bpy.context.active_object
+obj0.name = "cylinder"
+
+bpy.context.view_layer.objects.active.select_set(False)
+bpy.context.view_layer.objects.active = obj1
+bpy.context.view_layer.objects.active.select_set(True)
+
+bool0 = obj1.modifiers.new(type="BOOLEAN", name="bool0")
+bool0.object = obj0
+bool0.operation = 'DIFFERENCE'
+
+bpy.ops.transform.resize(value=(1, 1, 0.4))
+
+bpy.context.view_layer.objects.active.select_set(False)
+bpy.context.view_layer.objects.active = obj1
+bpy.context.view_layer.objects.active.select_set(True)
+
+bpy.ops.object.modifier_apply(modifier=bool0.name)
+
+bpy.context.view_layer.objects.active.select_set(False)
+bpy.context.view_layer.objects.active = obj0
+bpy.context.view_layer.objects.active.select_set(True)
+
+bpy.ops.transform.resize(value=(0.8, 0.8, 0.5))
+# bpy.context.object.hide_set(True)
+
+areas  = [area for area in bpy.context.window.screen.areas if area.type == 'VIEW_3D']
+for area in areas:
+    with bpy.context.temp_override(
+        window=bpy.context.window,
+        area=area,
+        region=[region for region in area.regions if region.type == 'WINDOW'][0],
+        screen=bpy.context.window.screen
+    ):
+        bpy.ops.view3d.view_axis(type='TOP', align_active=False)
+        bpy.ops.view3d.view_persportho()
+
+
+
+# verts = []
+# verts.append([1, 1, 0])
+# verts.append([1, -1, 0])
+# verts.append([-1, 1, 0])
+# verts.append([0, 0, 1])
+# edges = []
+# faces = []
+# faces.append([1, 2, 3])
+# faces.append([1, 2, 0])
+# mesh = bpy.data.meshes.new(name = "mesh0")
+# mesh.from_pydata(verts, edges, faces)
+# obj = bpy.data.objects.new("object0", mesh)
+# bpy.context.scene.collection.objects.link(obj)
